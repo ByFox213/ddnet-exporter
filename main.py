14be51ee6eb755286404d5b44d2359e0f5972d4c
ByFox <byfox213@gmail.com>
2025-01-23 12:38:19 +0500

    Code revision

--- main.py
+++ main.py
@@ -1,50 +1,53 @@
 import asyncio
 import logging
-from typing import Optional, AsyncGenerator
 
 from ddapi import DDnetApi, Server
-from prometheus_client import Counter, Histogram, start_http_server, Gauge, pushadd_to_gateway, CollectorRegistry
+from prometheus_client import start_http_server, Gauge, pushadd_to_gateway
 
 from modals import Config
-from util import get_config
+from util import REGISTRY, get_config, status_request
 
 dd = DDnetApi()
-config: Config = get_config(Config)
 
-logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s: %(message)s', level=getattr(logging, config.log_level.upper()))
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s: %(message)s', level=logging.INFO)
 _log = logging.getLogger(__name__)
 
-registry = CollectorRegistry()
+labels = ["address", "gametype", "map", "name", "hasPassword", "max_clients"]
+server_online = Gauge(
+    'server_online',
+    'ddnet server online',
+    labels,
+    registry=REGISTRY
+)
+server_online_per_ip = Gauge(
+    'server_online_per_ip',
+    'ddnet server online per ip',
+    ["address"],
+    registry=REGISTRY
+)
 
-count_request = Counter('count_request', 'request count', registry=registry)
-request_latency_seconds = Histogram('request_latency_seconds', 'histogram', registry=registry)
 
-servers = []
 
-labels = ["address", "gametype", "map", "name", "hasPassword"]
-server_online = Gauge('server_online', 'ddnet server online', labels, registry=registry)
-server_online_max = Gauge('server_online_max', 'ddnet server online max', labels, registry=registry)
+async def update_metrics(server: Server, addr: tuple[str, str]) -> None:
+    args = {
+        "address": f"{addr[0]}:{addr[1]}",
+        "map": server.info.map.name,
+        "hasPassword": str(server.info.passworded).lower(),
+        "gametype": server.info.game_type,
+        "name": server.info.name,
+        "max_clients": server.info.max_clients
+    }
+    online = len(server.info.clients)
 
-server_online_per_ip = Gauge('server_online_per_ip', 'ddnet server online per ip', ["address"], registry=registry)
-server_online_per_ip_max = Gauge('server_online_per_ip_max', 'ddnet server online per ip max', ["address"], registry=registry)
-
-@request_latency_seconds.time()
-async def status_request(_dd: DDnetApi, addresses: list) -> Optional[AsyncGenerator[Server, None]]:
-    count_request.inc()
-    request = await _dd.master()
-    if request is None:
-        yield [], None
-
-    for server in request.servers:
-        for ip, port in addresses:
-            addr = server.addresses[0].replace("tw-0.6+udp://", "").split(":")
-            if addr[0] == ip and (port is None or addr[1] == port):
-                yield addr, server
-
-    yield [], None
+    server_online_per_ip.labels(addr[0]).inc(online)
+    server_online.labels(**args).set(online)
 
 
 async def main():
+    config: Config = get_config(Config)
+    if config is None:
+        return ValueError("config is None")
+
     addresses = [
         (ip_port[0], int(ip_port[1]) if len(ip_port) > 1 else None)
         for ip_port in (
@@ -55,43 +58,22 @@ async def main():
     if config.gateway_address is not None:
         _log.info('| Push gateway client scheduled')
     else:
-        start_http_server(config.port, registry=registry)
+        start_http_server(config.port, registry=REGISTRY)
         _log.info("| Starting http server")
 
     while True:
         server_online_per_ip.clear()
-        server_online_per_ip_max.clear()
-
         server_online.clear()
-        server_online_max.clear()
-
-        servers.clear()
 
         async for addr, server in status_request(dd, addresses):
             if server is None:
                 await asyncio.sleep(config.sleep)
                 break
 
-            args = {
-                "address": f"{addr[0]}:{addr[1]}",
-                "map": server.info.map.name,
-                "hasPassword": str(server.info.passworded).lower(),
-                "gametype": server.info.game_type,
-                "name": server.info.name
-            }
-            servers.append(args)
-
-            ip = server.addresses[0].replace("tw-0.6+udp://", "")
-            online = len(server.info.clients)
-
-            server_online_per_ip.labels(ip).inc(online)
-            server_online_per_ip_max.labels(ip).inc(server.info.max_clients)
-
-            server_online.labels(**args).set(online)
-            server_online_max.labels(**args).set(server.info.max_clients)
+            await update_metrics(server, addr)
 
         if config.gateway_address is not None:
-            pushadd_to_gateway(config.gateway_address, job='ddnet-exporter', registry=registry)
+            pushadd_to_gateway(config.gateway_address, job='ddnet-exporter', registry=REGISTRY)
         await asyncio.sleep(config.sleep)
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -1,15 +1,35 @@
 import logging
+from collections.abc import AsyncGenerator
 
 import yaml
-
+from ddapi import DDnetApi, Server
+from prometheus_client import CollectorRegistry, Summary
 
 _log = logging.getLogger(__name__)
 
+REGISTRY = CollectorRegistry()
+request_latency_seconds = Summary(
+    'request_latency_seconds',
+    'histogram',
+    registry=REGISTRY
+)
 
 def get_config(modal):
     with open('config.yaml', encoding="utf-8") as fh:
         data = yaml.load(fh, Loader=yaml.FullLoader)
     _yaml = modal(**data) if data is not None else None
-    if _yaml is not None:
-        _log.info("config loaded from yaml")
-        return _yaml
+    return _yaml
+
+@request_latency_seconds.time()
+async def status_request(_dd: DDnetApi, addresses: list) -> AsyncGenerator[Server] | None:
+    request = await _dd.master()
+    if request is None:
+        yield [], None
+
+    for server in request.servers:
+        for ip, port in addresses:
+            addr = server.addresses[0].replace("tw-0.6+udp://", "").split(":")
+            if addr[0] == ip and (port is None or addr[1] == port):
+                yield addr, server
+
+    yield [], None
